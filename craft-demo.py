970359a7d6b3ff62a3736d0a88e7d872bbe5f0e2
Scott O <soppenlander@gmail.com>
2015-08-10 20:34:16 -0500

    adding sample apps 1.0 and 2.0.  Salt works now.

--- craft-demo.py
+++ craft-demo.py
@@ -62,21 +62,31 @@ def get_external_ip():
     address = grab[0]
     return address;
 
-# Want to make sure SSH (port 22) and salt (4505 and 4506) are open.
-def check_security_group():
+# Want to make sure SSH (port 22) is open to my IP address
+def security_group_ssh():
 
-	# If the demo security group exists, delete and re-create so it's known to be clean.
+	# If the demo security group exists, purge so it's known to be clean.
 	groups = ec2.get_all_security_groups()
 	for group in groups:
 		if group.name == DEMO_SECURITY_GROUP:
 			ec2.delete_security_group(DEMO_SECURITY_GROUP)
 
-	# create new security group opening up the right ports
+	# create new security group opening up ssh and tomcat7 ports
 	group = ec2.create_security_group(DEMO_SECURITY_GROUP, 'Security group for demonstration')
 	my_cidr = get_external_ip() + '/32'
 	group.authorize('tcp', 22, 22, my_cidr)
-	group.authorize('tcp', 4505, 4505, my_cidr)
-	group.authorize('tcp', 4506, 4506, my_cidr)
+	group.authorize('tcp', 8080, 8080, my_cidr)
+	return;
+
+# open ports 4505 and 4506 on the salt master.
+def security_group_salt():
+
+	groups = ec2.get_all_security_groups()
+	for group in groups:
+		if group.name == DEMO_SECURITY_GROUP:
+			for i in range(1, DEMO_MINIONS+1):	
+				group.authorize('tcp', 4505, 4506, hosts[i].private_ip_address + '/32')
+
 	return;
 
 # copy hostnames of running instances into the hosts list
@@ -121,10 +131,33 @@ def install_salt_instances():
 
 	# install the salt master package on the salt master, [0] by convention
 	subprocess.call([SSH, SSH_FLAGS, hosts[0].public_dns_name, "sudo apt-get install -y salt-master"])
+	subprocess.call([SSH, SSH_FLAGS, hosts[0].public_dns_name, "sudo service salt-master start"])
+
 
 	# install salt minion on the rest of them
 	for i in range(1, DEMO_MINIONS+1):
 		subprocess.call([SSH, SSH_FLAGS, hosts[i].public_dns_name, "sudo apt-get install -y salt-minion"])
+		subprocess.call([SSH, SSH_FLAGS, hosts[i].public_dns_name, "sudo chmod 666 /etc/hosts"])
+		subprocess.call([SSH, SSH_FLAGS, hosts[i].public_dns_name, "sudo echo \"%s %s salt\" >> /etc/hosts" % (hosts[0].private_ip_address, hosts[0].private_dns_name)])
+		subprocess.call([SSH, SSH_FLAGS, hosts[i].public_dns_name, "sudo chmod 444 /etc/hosts"])
+		subprocess.call([SSH, SSH_FLAGS, hosts[i].public_dns_name, "sudo service salt-minion start"])
+
+	return;
+
+# use the salt-master to install jdk and tomcat7 on the minions.  also sign the minion keys.
+def install_jdk_tomcat():
+
+	subprocess.call([SSH, SSH_FLAGS, hosts[0].public_dns_name, "sudo salt-key -A -y"])
+	subprocess.call([SSH, SSH_FLAGS, hosts[0].public_dns_name, "sudo apt-get install default-jdk -y"])
+	subprocess.call([SSH, SSH_FLAGS, hosts[0].public_dns_name, "sudo apt-get install tomcat7 tomcat7-admin tomcat7-user -y"])
+
+	return;
+
+# copy sample war files out to the salt master, then tell salt master to deploy to the minions
+def deploy_sample():
+	subprocess.call(["scp", "sample-1.0/sample-1.0.war", "ubuntu@%s:/tmp" % hosts[0].public_dns_name])
+	subprocess.call(["scp", "sample-2.0/sample-2.0.war", "ubuntu@%s:/tmp" % hosts[0].public_dns_name])
+	subprocess.call([SSH, SSH_FLAGS, hosts[0].public_dns_name, "sudo salt-cp '*' /tmp/sample-1.0.war /var/lib/tomcat7/webapps"])
 
 	return;
 
@@ -138,7 +171,7 @@ ec2 = boto.ec2.connect_to_region(os.environ['AWS_DEFAULT_REGION'], aws_access_ke
 
 # set up security group
 print "Configuring security groups..."
-#check_security_group()
+#security_group_ssh()
 
 # start up a salt master and N minions, add one for the salt master
 print "Starting instances..."
@@ -146,7 +179,7 @@ print "Starting instances..."
 #	ec2.run_instances(DEMO_AMI, key_name='demo-key-pair', instance_type='t1.micro', security_groups=[DEMO_SECURITY_GROUP])
 
 # check once every 30 seconds until they're all fully up
-#wait_for_servers_up()
+wait_for_servers_up()
 
 # shortcut to have all the running servers
 print "Setting up hosts list..."
@@ -154,17 +187,21 @@ load_hosts_list()
 
 # tag server[0] as the salt master and server[1..N] as salt minions out of running instances
 print "Tagging salt hosts..."
-tag_salt_hosts()
+#tag_salt_hosts()
 
-# install salt master and salt minion packages
+# open up salt ports on the salt master
+print "More configuration of security groups..."
+#security_group_salt()
+
+# install salt master and salt minion packages, update host files
 print "Installing salt..."
-install_salt_instances()
+#install_salt_instances()
 
-# TODO:  update host files and salt config
+# install jdk tomcat through salt
+#install_jdk_tomcat()
 
-# TODO:  install tomcat through salt
-
-# TODO:  deploy hello world app through salt
+# copy sample war files to the salt master and deploy 1.0 through salt
+deploy_sample()
 
 # TODO:  create load balancer, add salt minions to it
 
---
+++ sample-2.0/hello.jsp
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>New and Improved JSP Page!</title>
+</head>
+<body bgcolor=white>
+
+<table border="0">
+<tr>
+<td align=center>
+<img src="images/coyote.jpg">
+</td>
+<td>
+<h1>Sample Application JSP Page</h1>
+This is the output of a JSP page that is part of the Hello, World application,
+version 2.0.
+</td>
+</tr>
+</table>
+
+<%= new String("Hello again!") %>
+
+</body>
+</html>
